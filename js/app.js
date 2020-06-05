cdf0a62000235407b9fd4db1831a2015cd3abcb9
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-06-05 12:11:03 -0400

    Update app.js
    Create for loop to iterate over first 12 products and add info to the product-info div

--- js/app.js
+++ js/app.js
@@ -15,3 +15,38 @@ request.onload = function () {
 
 // declare const for div that will contain product info
 const div = document.querySelector('#product-card');
+
+function populateInfo(jsonObj) {
+	const products = jsonObj['products'];
+
+	// loop through first 12 products from the JSON file
+	for (let i = 0; i < 12; i++) {
+		// create product info div inside
+		var productInfoDiv = document.createElement('div');
+		productInfoDiv.className = 'product-info';
+		document
+			.getElementById('product-container')
+			.appendChild(productInfoDiv);
+
+		var image = new Image();
+		image.src = jsonObj['products'][i]['images'][0]['src'];
+		productInfoDiv.appendChild(image);
+
+		const vendor = document.createElement('h3');
+		vendor.textContent = jsonObj['products'][i]['vendor'];
+		productInfoDiv.appendChild(vendor);
+
+		const price = document.createElement('h4');
+		price.textContent =
+			'$' + jsonObj['products'][i]['variants'][0]['price'];
+		productInfoDiv.appendChild(price);
+
+		const tags = document.createElement('span');
+		// format the tags to add hardcoded superscript '2' and join array with comma and space
+		const formattedTags = jsonObj['products'][i]['tags']
+			.map((tag) => tag.replace('<sup>2</sup>', `²`))
+			.join(', ');
+		tags.textContent = 'Tags: ' + formattedTags;
+		productInfoDiv.appendChild(tags);
+	}
+}
